14b1c907e8188f56faff35632cfc9baae3a34b4c
Elijah Cleveland <elicleveland12@gmail.com>
2020-05-07 15:36:01 -0400

    removed event listener

--- src/hooks/screen-to-geo.ts
+++ src/hooks/screen-to-geo.ts
@@ -1,18 +1,18 @@
 import React from 'react';
 import MapContext from '../utils/map-context';
-const mapContext = React.useContext(MapContext);
-const { map } = mapContext;
 
-function screenToGeo() {
-  if (map) {
-    map.addEventListener('tap', (event: any) => {
+export function screenToGeo(event: any) {
+  const mapContext = React.useContext(MapContext);
+
+  React.useEffect(() => {
+    const { map } = mapContext;
+
+    if (map) {
       const coords = map.screenToGeo(
         event.currentPointer.viewportX,
         event.currentPointer.viewportY,
       );
       console.log(coords);
-    });
-  }
+    }
+  });
 }
-
-export { screenToGeo };
