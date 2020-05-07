29edd4adb71f8153c3dd53c019ba64adcdf953e0
Elijah Cleveland <elicleveland12@gmail.com>
2020-05-07 12:30:51 -0400

    removed screentogeo

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,6 @@ import Circle from './Circle';
 import Marker from './Marker';
 import RouteLine from './RouteLine';
 import { usePlatform } from './hooks/use-platform';
-import { screenToGeo } from './utils/screen-to-geo';
-export { Circle, HEREMap, Marker, RouteLine, usePlatform, screenToGeo };
+export { Circle, HEREMap, Marker, RouteLine, usePlatform };
 
 export default HEREMap;
