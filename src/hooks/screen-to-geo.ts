994e9cbf4a428aa28a4b5faf25c4ed05b75f0c43
Elijah Cleveland <elicleveland12@gmail.com>
2020-05-07 15:40:25 -0400

    removed event listener

--- src/hooks/screen-to-geo.ts
+++ src/hooks/screen-to-geo.ts
@@ -3,10 +3,9 @@ import MapContext from '../utils/map-context';
 
 export function screenToGeo(event: any) {
   const mapContext = React.useContext(MapContext);
+  const { map } = mapContext;
 
   React.useEffect(() => {
-    const { map } = mapContext;
-
     if (map) {
       const coords = map.screenToGeo(
         event.currentPointer.viewportX,
