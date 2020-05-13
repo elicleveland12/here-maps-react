974d9fffbf61f48181269b1d926cd069e1d88618
Elijah Cleveland <elicleveland12@gmail.com>
2020-05-13 10:23:20 -0400

    resetting marker state instead of removeobject

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -57,7 +57,7 @@ export const Marker: React.FC<MarkerProps> = ({
     }
     return () => {
       if (map && marker) {
-        // map.removeObject(marker);
+        setMarker(undefined);
       }
     };
   }, [bitmap, children, draggable, lat, lng, mapContext, marker]);
