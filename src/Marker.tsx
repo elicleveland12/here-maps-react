023cc78e17991cae3987ef0c91e7c17aa7fa27ec
Elijah Cleveland <elicleveland12@gmail.com>
2020-05-13 11:39:58 -0400

    added some marker logging

--- src/Marker.tsx
+++ src/Marker.tsx
@@ -59,6 +59,7 @@ export const Marker: React.FC<MarkerProps> = ({
     }
     return () => {
       if (map && marker) {
+        console.log(markerGroup);
         markerGroup.removeAll();
       }
     };
